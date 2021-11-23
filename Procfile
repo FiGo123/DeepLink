f97e70566d6f2490ec364e6f503d95ec2fe50b04
Filip Golovic <filipgolovic@Administrators-MacBook-Pro.local>
2021-11-23 19:59:26 +0100

    Add procfile and requirements

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn deeplink.wsgi
--- deeplink/settings.py
+++ deeplink/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-74a795sor-0yk_zm5nt^uf^#jrst%pa+vzvy7onkzf^d-!ahpg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
